38d35f220b64c89c848897db820323048bf0c675
appelmoesHACK <101522959+appelmoesHACK@users.noreply.github.com>
2023-01-21 17:31:12 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,10 +1,10 @@
 # copyright appelmoesGG
 
 import os
-os.chdir('/mnt/chromeos/MyFiles/klanken/orkestklanken')
+os.chdir('/mnt/chromeos/MyFiles/klanken/Instrumenten')
 
 for dir in os.listdir():
-    os.chdir('/home/appelmoes/Documents/klanken/orkestklanken/' + dir)
+    os.chdir('/home/appelmoes/Documents/klanken/Instrumenten/' + dir)
     for filename in os.listdir():
         f = filename
         if os.path.isfile(f):
@@ -13,5 +13,5 @@ for dir in os.listdir():
            os.system('mpg321 -q -w ' +  nomp3 + ".wav " + f)
            print('Converted ' + f)
     os.system('rm *.mp3')
-    os.chdir('/home/appelmoes/Documents/klanken/orkestklanken/')
+    os.chdir('/home/appelmoes/Documents/klanken/Instrumenten/')
     print('Finsished with ' + dir)
